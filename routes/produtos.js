f5060958e4b5d511de61dcf4b7fc3bdeb3af02b8
Thiago Vinicius <tviniciusas@gmail.com>
2021-01-28 21:20:34 -0300

    api criada

---
+++ routes/produtos.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res, next) =>{
+
+    res.status(200).send({
+        message: 'usando rota de produtos'
+    })
+})
+
+
+router.post('/', (req, res, next) =>{
+
+    res.status(201).send({
+        mensagem: 'usando post rota produtos'
+    })
+})
+
+module.exports = router
