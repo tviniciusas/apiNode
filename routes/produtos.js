7c5f72ce90a20ce31dcb34b881ba320502f92611
Thiago Vinicius <tviniciusas@gmail.com>
2021-01-30 21:33:12 -0300

    saporra saiu caralho

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) =>{ res.status(200).send({ message: 'usando rota de produtos e testando heroku' }) })
+router.get('/', (req, res, next) =>{ res.status(200).send({ message: 'usando get com servidor proprio porra!!! uhulll' }) })
 
-router.post('/', (req, res, next) =>{ res.status(201).send({ mensagem: 'usando post rota produtos' }) })
+router.post('/', (req, res, next) =>{ res.status(201).send({ mensagem: 'usando post servidor proprio porra!!! uhulll' }) })
 
 module.exports = router
