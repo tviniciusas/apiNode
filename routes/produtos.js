c0d9bfbda35e0510685828f029948bbae0e91d8d
Thiago Vinicius <tv.dclxvii@gmail.com>
2021-02-11 16:49:54 +0000

    teste

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) =>{ res.status(200).send({ message: 'usando get com servidor proprio porra!!! uhulll' }) })
+router.get('/', (req, res, next) =>{ res.status(200).send({ message: 'usando get teste de alteração' }) })
 
 router.post('/', (req, res, next) =>{ res.status(201).send({ mensagem: 'usando post servidor proprio porra!!! uhulll' }) })
 
